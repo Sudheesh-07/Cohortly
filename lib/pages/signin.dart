b3b8aa86434a1dc8c19471ee0b8bc821f80b7e8f
pritisingh-09 <pritisingh0904@gmail.com>
2024-02-25 18:04:07 +0530

    signin ver 1.3

--- lib/pages/signin.dart
+++ lib/pages/signin.dart
@@ -45,7 +45,7 @@ class _SignInState extends State<SignIn> {
                             color: Colors.white,
                             fontSize: 24.0,
                             fontWeight: FontWeight.bold), //textstyle
-                      )), //text //center
+                      )),
                   Center(
                       child: Text(
                         "Login to your account",
@@ -53,27 +53,110 @@ class _SignInState extends State<SignIn> {
                             color: Color(0xFFbbb0ff),
                             fontSize: 18.0,
                             fontWeight: FontWeight.w500), //textstyle
-                      )), //text //center
+                      )),
+                  SizedBox(height: 20.0,),
                   Container(
-                    margin: EdgeInsets.symmetric(vertical: 20.0),
-                    height: MediaQuery
-                        .of(context)
-                        .size
-                        .height / 2,
-                    width: MediaQuery
-                        .of(context)
-                        .size
-                        .width,
-                    decoration: BoxDecoration(color: Colors.white,
-                        borderRadius: BorderRadius.circular(10)),
-                  ) //Container
+                    margin: EdgeInsets.symmetric(vertical: 20.0,horizontal: 20.0),
+                    child: Material(
+                      elevation: 5.0,
+                      borderRadius: BorderRadius.circular(10),
+                      child : Container(
+                        padding: EdgeInsets.symmetric(vertical:30.0, horizontal: 20.0),
+                        height: MediaQuery.of(context).size.height / 2,
+                        width: MediaQuery.of(context).size.width,
+                        decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(10)),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                      Text(
+                        "Email",
+                        style:TextStyle(
+                            color:Colors.black,
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.w500 ),),
+                      SizedBox(height: 10.0,),
+                      Container(
+
+                        decoration: BoxDecoration(
+                            border: Border.all(width: 1.0, color: Colors.black38)),
+                        child: TextField(
+  decoration: InputDecoration(border: InputBorder.none,prefixIcon: Icon(Icons.mail_outline,color: Color(0xFF7f30fe),))
+
+                      ),),
+                        SizedBox(height: 20.0,),
+                        Text(
+                          "Password",
+                          style:TextStyle(
+                              color:Colors.black,
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.w500 ),),
+                        SizedBox(height: 10.0,),
+                        Container(
+
+                          decoration: BoxDecoration(
+                              border: Border.all(width: 1.0, color: Colors.black38)),
+                          child: TextField(
+                              decoration: InputDecoration(
+                                  border: InputBorder.none,
+
+                                  prefixIcon: Icon(
+                                      Icons.password,
+                                      color: Color(0xFF7f30fe),
+                                  )),
+                            obscureText: true,
+
+                          ),),
+                        SizedBox(height:10.0,),
+                        Container(
+                          alignment: Alignment.bottomRight,
+                          child: Text(
+                            "Forgot Password?",
+                            style:TextStyle(
+                                color:Colors.black,
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                        SizedBox(height: 50.0,),
+                        Center(
+                          child: Container(
+                            width: 130,
+                            child: Material(
+                              elevation: 5.0,
+                                borderRadius: BorderRadius.circular(10),
+                              child: Container(
+
+                                  padding: EdgeInsets.all(10),
+                                  decoration:BoxDecoration(color: Color(0xFF6380fb),borderRadius: BorderRadius.circular(10)),
+                                  child: Center(child: Text("SignIn",style: TextStyle(color: Colors.white, fontSize: 18.0, fontWeight: FontWeight.bold),)),),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),),
+                    ),
+                  ),
+                 SizedBox(height: 40.0,),
+                 Row(
+                   mainAxisAlignment: MainAxisAlignment.center,
+                   children: [
+                     Text(
+                       "Don't have an account?",
+                       style: TextStyle(color: Colors.black,fontSize: 16.0),
+                 ),
+                     Text(
+                         " Sign Up Now!",
+                         style: TextStyle(color: Color(0xFF7f30fe),fontSize: 16.0, fontWeight: FontWeight.w500),
+                     )
+                   ],
+                 )
                 ],
-              ), //column
-            ) //padding
+              )
+            ) ],
+              ),
+            )
           ],
-        ), //Stack
-      ), //Container
-    ); //Scaffold
+        );
   }
 }
 
